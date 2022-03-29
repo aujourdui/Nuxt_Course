fd52214e3460a9ac24292ee214aac059c5026992
kyosuke <kyosuke.ito@gmail.com>
2022-03-28 19:55:14 -0700

    finish assignment route

--- pages-views-assignment-problem/pages/index.vue
+++ pages-views-assignment-problem/pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="container">
-    <ol>
-      <li>Create two routes: /products and /products/:pid</li>
-      <li>Add a button to the main page. The button should navigate (programmatically) to /products</li>
+    <!-- <ol> -->
+      <!-- <!-- <li>Create two routes: /products and /products/:pid</li> -- -->
+      <!-- <a href="/products">Go to products</a> -->
+      <!-- <li>Add a button to the main page. The button should navigate (programmatically) to /products</li>
       <li>Add two links to the /products page. The links should go to /products/1 and /products/2</li>
-      <li>Output the product id on the /products/:pid page</li>
-    </ol>
+      <li>Output the product id on the /products/:pid page</li> -->
+    <!-- </ol> -->
+    <button @click="$router.push('/products')">button</button>
+    <!-- <button @click="$router.push("/products")">button</button> -->
   </section>
 </template>
 
---
+++ pages-views-assignment-problem/pages/products/_pid/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is each product page! {{ $route.params.pid }}</h1>
+</template>
